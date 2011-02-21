4bdc0dc95722211f6fe3de8507d296b2d2466277
harj <harjeet.taggar@gmail.com>
2011-02-20 18:15:55 -0800

    adding in migration files

---
+++ db/migrate/20110221013411_add_synced_to_hours.rb
@@ -0,0 +1,9 @@
+class AddSyncedToHours < ActiveRecord::Migration
+  def self.up
+    add_column :hours, :synced, :boolean, :default => 'false'
+  end
+
+  def self.down
+    remove_column :hours, :synced
+  end
+end
