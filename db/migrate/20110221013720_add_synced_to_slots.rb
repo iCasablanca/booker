9f5f2447c46d0458babd2af4f9e5081619e70a9c
harj <harjeet.taggar@gmail.com>
2011-02-20 18:15:55 -0800

    adding in migration files

---
+++ db/migrate/20110221013720_add_synced_to_slots.rb
@@ -0,0 +1,9 @@
+class AddSyncedToSlots < ActiveRecord::Migration
+  def self.up
+    add_column :slots, :synced, :boolean, :default => 'false'
+  end
+
+  def self.down
+    remove_column :slots, :synced
+  end
+end
